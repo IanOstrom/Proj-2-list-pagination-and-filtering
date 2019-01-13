ddaabb0070c988f647cd7e1c3c603c508609faa2
Ostrom <iostrom@IOSTROMMBP-2.local>
2019-01-13 14:45:25 -0500

    Pagination functionality added

--- js/script.js
+++ js/script.js
@@ -2,49 +2,74 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
+
+const studentUl = document.querySelector('.student-list');
+const list = studentUl.children;
 
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+//Hides all of the items in the list except for the ten on the selected page.
 
+function showPage(list, page){
+   const min = (page + 1) * 10 - 10;
+   const max = (page + 1) * 10;
+   for (let i=0; i<list.length; i++){
+      if(i>=min && i<max){
+         list[i].style.display = '';
+      } else{
+         list[i].style.display = 'none';
+      }
+   }
+}
 
+// Creates a new HTML tag and appends it to the given parent element.
 
+function addElement(tagName, parent){
+   let newElement = document.createElement(tagName);
+   parent.appendChild(newElement);
+   return newElement;
+}
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
+// Generates and appends the pagination links to the bottom of the page.
+// Adds an event listener to update the displayed list of students when a link is clicked.
 
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
+function appendPageLinks(){
+   const linksDiv = addElement('div', studentUl.parentNode);
+   linksDiv.className = 'pagination';
+   const linksList = addElement('ul', linksDiv);
+   const pages = Math.ceil(list.length/10);
 
+   // Sets the clicked page number as the active page.
+   // Updates the list of students shown by calling showPage.
 
+   function setActiveLink(activePage){
+      for(let i=0; i<pages; i++){
+         const link = linksList.children[i].firstElementChild;
+         if (i===activePage){
+            link.className = 'active';
+         } else{
+            link.className = '';
+         }
+      }
+      showPage(list, activePage);
+   }
 
+   // Creates each link and adds an event listener.
+   for (let i=0; i<pages; i++){
+      let pageLi = addElement('li', linksList);
+      let link = addElement('a', pageLi);
+      link.href = '#';
+      link.textContent = i+1;
 
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
+      // Sets the first page as active on initial page load.
+      if (i===0){
+         link.className = 'active';
+      } 
+      link.addEventListener('click', ()=>{
+         setActiveLink(i);
+      });
+   }
+}
 
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+// Creates the pagination and shows the first page on initial page load.
+appendPageLinks();
+showPage(list, 0);
